a945e5b69481b34c884dc011b1d26d6b1f92cbf0
shurecho <m.shahid2486@gmail.com>
2023-05-14 16:07:04 +0500

    project structure created

---
+++ components/script.php
@@ -0,0 +1,11 @@
+<div class="progress-wrap">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+    </svg>
+</div>
+<script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script src="assets/js/plugins.js"></script>
+<script src="assets/js/theme.js"></script>
+</body>
+
+</html>
---
+++ structure.php
@@ -0,0 +1,6 @@
+<?php include "components/header.php";?>
+<div class="content-wrapper">
+    <?php include "components/nav.php";?>
+    <?php include "components/footer.php";?>
+</div>
+<?php include "components/script.php";?>
